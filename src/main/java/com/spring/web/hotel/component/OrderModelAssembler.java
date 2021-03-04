59a211b9f54f2758326b96b9d27ecd3878f1f46a
jordi <jordikiter@hotmail.es>
2021-03-04 23:56:25 +0000

    Order controller

---
+++ src/main/java/com/spring/web/hotel/component/OrderModelAssembler.java
@@ -0,0 +1,28 @@
+package com.spring.web.hotel.component;
+
+import com.spring.web.hotel.controller.OrderController;
+import com.spring.web.hotel.model.Order;
+import com.spring.web.hotel.model.Status;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>> {
+    @Override
+    public EntityModel<Order> toModel(Order entity) {
+        EntityModel<Order> orderModel = EntityModel.of(entity,
+                linkTo(methodOn(OrderController.class).one(entity.getId())).withSelfRel(),
+                linkTo(methodOn(OrderController.class).all()).withRel("orders"));
+
+        if (entity.getStatus() == Status.IN_PROGRESS) {
+            orderModel.add(linkTo(methodOn(OrderController.class).cancel(entity.getId())).withRel("cancel"));
+            orderModel.add(linkTo(methodOn(OrderController.class).complete(entity.getId())).withRel("complete"));
+        }
+
+        return orderModel;
+    }
+}
